f215f189a0f1850dcc2649a8c7fd2161a21f5b7b
ShoppingLiao <tom0801a@gmail.com>
2020-05-04 22:39:42 +0800

    Updates

---
+++ precache-manifest.070fe9b7e0237eb4e6ba325ac4a80777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1dfe613c180a55b86d4b97a64cb512",
+    "url": "/Simple-Youtube-Search-List/index.html"
+  },
+  {
+    "revision": "41ba55367f9a9405c16e",
+    "url": "/Simple-Youtube-Search-List/static/css/main.b26faf39.chunk.css"
+  },
+  {
+    "revision": "0324428ca4241cf4aa71",
+    "url": "/Simple-Youtube-Search-List/static/js/2.a4391c6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-Youtube-Search-List/static/js/2.a4391c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ba55367f9a9405c16e",
+    "url": "/Simple-Youtube-Search-List/static/js/main.e26a2b6d.chunk.js"
+  },
+  {
+    "revision": "7af46025e43ed21b271d",
+    "url": "/Simple-Youtube-Search-List/static/js/runtime-main.274c3135.js"
+  }
+]);
