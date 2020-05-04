7dfc529baaecf6a673464ed4efe59aa522015f09
ShoppingLiao <tom0801a@gmail.com>
2020-05-04 22:36:47 +0800

    feat: 功能初版

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,85 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Wrapper, Header, Content } from './Styled';
+import { Wrapper, Header, Content, Item, Footer, PageBtn } from './Styled';
 
 export default function Main() {
+  const [searchKey, setSearchKey] = useState('');
+  const [pageData, setPageData] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function handleSearchKeyChange(value) {
+    setSearchKey(value);
+  }
+
+  function handleSearch() {
+    if (searchKey.trim().length === 0) {
+      return;
+    }
+    fetchData();
+  }
+
+  async function fetchData() {
+    const key = 'YOUR_API_KEY';
+    const res = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?type=video&maxResults=30&key=${key}&part=snippet&q=${searchKey}`
+    );
+
+    const newPageData = {
+      page1: [],
+      page2: [],
+      page3: [],
+    };
+    res.data.items.forEach((item, idx) => {
+      if (idx < 10) {
+        newPageData.page1.push(item);
+      } else if (idx < 20) {
+        newPageData.page2.push(item);
+      } else {
+        newPageData.page3.push(item);
+      }
+    });
+
+    setPageData(newPageData);
+  }
+
+  function handleOpenVedio(id) {
+    window.open(`https://www.youtube.com/watch?v=${id}`);
+  }
+
   return (
     <Wrapper>
       <Header>
-        <input type='text' placeholder='輸入關鍵字' />
+        <div>
+          <input
+            type='text'
+            placeholder='輸入關鍵字'
+            value={searchKey}
+            onChange={(e) => handleSearchKeyChange(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
       </Header>
-      <Content>這邊放結果</Content>
+      <Content>
+        {pageData &&
+          pageData[`page${currentPage}`].map(({ id, snippet }) => (
+            <Item key={id.videoId} onClick={() => handleOpenVedio(id.videoId)}>
+              <img src={snippet.thumbnails.high.url} alt='test' />
+              <div>{snippet.title}</div>
+            </Item>
+          ))}
+      </Content>
+      <Footer>
+        {pageData &&
+          [1, 2, 3].map((page) => (
+            <PageBtn
+              key={`page_${page}`}
+              onClick={() => setCurrentPage(page)}
+              actived={currentPage === page}
+            >
+              {page}
+            </PageBtn>
+          ))}
+      </Footer>
     </Wrapper>
   );
 }
--- src/Components/Main/Styled.js
+++ src/Components/Main/Styled.js
@@ -11,5 +11,69 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Header = styled.div``;
-export const Content = styled.div``;
+export const Header = styled.div`
+  background: #ff5956;
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  justify-content: center;
+
+  div {
+    border-bottom: 1px solid #fff;
+  }
+  input {
+    border: none;
+    color: #fff;
+    &::placeholder {
+      color: #fff;
+    }
+  }
+  button {
+    font-size: 0.5rem;
+    padding: 0 0.2rem;
+    height: 0.8rem;
+    line-height: 0.8rem;
+    color: #fff;
+    background: none;
+  }
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Item = styled.div`
+  margin: 0.2rem;
+  width: 20vw;
+  cursor: pointer;
+  user-select: none;
+  @media (max-width: 1024px) {
+    width: 43vw;
+  }
+  img {
+    margin: 0 auto;
+    display: block;
+    width: 100%;
+  }
+  div {
+    display: block;
+    font-size: 0.5rem;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const PageBtn = styled.button`
+  color: ${(props) => (props.actived ? '#fff' : '#666')};
+  background: ${(props) => (props.actived ? ' #ff5956' : 'transparent')};
+  border-radius: 100%;
+  height: 1rem;
+  width: 1rem;
+  margin: 0.2rem;
+`;
